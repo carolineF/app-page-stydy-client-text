94b0fb79e62bc1b9f097754094048d241351c207
wangfengzhu <wangfengzhu@meituan.com>
2017-07-22 12:52:19 +0800

    update

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     swPrecache: swPrecacheConfig,
     build: {
         env: require('./prod.env.js'),
-        index: path.resolve(__dirname, '../dist/index.html'),
-        assetsRoot: path.resolve(__dirname, '../dist'),
+        index: path.resolve(__dirname, '../docs/index.html'),
+        assetsRoot: path.resolve(__dirname, '../docs'),
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
         productionSourceMap: true,
